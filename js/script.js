8b999d882a93c15fdb47d138765593b98e2ca56f
Jonathan-Ultini <jonathanultini@gmail.com>
2024-07-31 14:40:38 +0200

    better style, better naming

--- js/script.js
+++ js/script.js
@@ -8,30 +8,27 @@ console.log('Età:', age);
 
 // #: Fase di elaborazione
 // Calcola il prezzo del biglietto senza sconto
-const prezzoPerKm = 0.21;
-let prezzoTotale = km * prezzoPerKm;
-const prezzoSenzaSconto = prezzoTotale.toFixed(2); //fissaggio a due decimali
-console.log('Prezzo senza sconto:', prezzoSenzaSconto);
+const pricePerKm = 0.21;
+let totalPrice = km * pricePerKm;
+const priceNoDiscount = totalPrice.toFixed(2); //fissaggio a due decimali
+console.log('Prezzo senza sconto:', priceNoDiscount);
 
 // Applica lo sconto in base all'età
 if (age < 18) {
   // Sconto del 20% per i minorenni
-  prezzoTotale *= 0.8;
+  totalPrice *= 0.8;
 } else if (age > 65) {
   // Sconto del 40% per gli over 65
-  prezzoTotale *= 0.6;
+  totalPrice *= 0.6;
 }
 
 // Format il prezzo a due decimali
-const prezzoFinale = prezzoTotale.toFixed(2);
-console.log('Prezzo finale:', prezzoFinale);
+const finalPrice = totalPrice.toFixed(2);
+console.log('Prezzo finale:', finalPrice);
 
 // #: Fase di Output
 // Mostra il risultato sulla pagina
-const resultDiv = document.getElementById('result');
-resultDiv.innerHTML = `
-      <p>Chilometri: ${km}</p>
-      <p>Età: ${age}</p>
-      <p>Prezzo senza sconto: €${prezzoSenzaSconto}</p>
-      <p>Prezzo finale: €${prezzoFinale}</p>
-    `;
+document.getElementById('km').textContent = km;
+document.getElementById('age').textContent = age;
+document.getElementById('priceNoDiscount').textContent = priceNoDiscount;
+document.getElementById('finalPrice').textContent = finalPrice;

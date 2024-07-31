52cc856a424f8d84acc3510fc2916ea5da3fa263
Jonathan-Ultini <jonathanultini@gmail.com>
2024-07-31 14:15:39 +0200

    begin phase 2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ console.log('Chilometri:', km);
 
 const age = prompt("Qual è l'età del passeggero?", '30');
 console.log('Età:', age);
+
+// #: Fase di elaborazione
+// Calcola il prezzo del biglietto senza sconto
+const prezzoPerKm = 0.21;
+let prezzoTotale = km * prezzoPerKm;
+const prezzoSenzaSconto = prezzoTotale.toFixed(2);
+console.log('Prezzo senza sconto:', prezzoSenzaSconto);

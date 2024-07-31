4aef654aebea786afd60a8c33df528aaef393eea
Jonathan-Ultini <jonathanultini@gmail.com>
2024-07-31 14:11:18 +0200

    fix html, add first phase

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("JS OK")
+// #: Fase raccolta dati
+// Chiediamo i dati all'utente
+const km = prompt('Quanti chilometri vuoi percorrere?', '100');
+console.log('Chilometri:', km);
+
+const age = prompt("Qual è l'età del passeggero?", '30');
+console.log('Età:', age);

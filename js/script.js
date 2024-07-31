59372c09618aa362794e4e5be626d8e5b085a5d9
Jonathan-Ultini <jonathanultini@gmail.com>
2024-07-31 14:17:49 +0200

    add discount and end phase 2

--- js/script.js
+++ js/script.js
@@ -10,5 +10,18 @@ console.log('Età:', age);
 // Calcola il prezzo del biglietto senza sconto
 const prezzoPerKm = 0.21;
 let prezzoTotale = km * prezzoPerKm;
-const prezzoSenzaSconto = prezzoTotale.toFixed(2);
+const prezzoSenzaSconto = prezzoTotale.toFixed(2); //fiisaggio a due decimali
 console.log('Prezzo senza sconto:', prezzoSenzaSconto);
+
+// Applica lo sconto in base all'età
+if (age < 18) {
+  // Sconto del 20% per i minorenni
+  prezzoTotale *= 0.8;
+} else if (age > 65) {
+  // Sconto del 40% per gli over 65
+  prezzoTotale *= 0.6;
+}
+
+// Format il prezzo a due decimali
+const prezzoFinale = prezzoTotale.toFixed(2);
+console.log('Prezzo finale:', prezzoFinale);
